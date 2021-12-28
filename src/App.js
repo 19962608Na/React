094967863874935d39d4ec585661963d19ba11a8
hang phuong <xna.giotnang@gmail.com>
2021-12-28 22:54:25 +0700

    edit(1)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import React, { Component } from 'react';
 import StaffList from './components/StaffListComponent';
 import './App.css';
-import { Component } from 'react';
+import {STAFFS} from './shared/staffs';
+
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      
+      staffs: STAFFS
     }
   }
   render() {
@@ -19,6 +21,7 @@ class App extends Component {
             <NavbarBrand href='#'>Ứng dụng quản lí nhân sự v1.0</NavbarBrand>
           </div>
         </Navbar>
+        <StaffList staffs={this.state.staffs}/>
       </div>
     );
   }
---
+++ src/components/StaffListComponent.js
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import {STAFFS} from '../shared/staffs';
+import {Card, CardText, CardBody, CardTitle} from 'reactstrap';
+
+class StaffList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+        const staffList = this.props.staffs.map((staff) => {
+            return (
+                <Card key={staff.id} className = "col-12 col-sm-6 col-md-4">
+                    <CardBody>
+                        <CardText>{staff.name}</CardText>
+                    </CardBody>
+                </Card>
+            )
+        })
+        return ( 
+            <div className="container">
+                <div className="row">
+                    {staffList}
+                </div>
+            </div>
+         );
+    }
+}
+ 
+export default StaffList;
