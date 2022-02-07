c1e5bb570719f48b178ee8f59c4ea78316e97a01
hang phuong <xna.giotnang@gmail.com>
2022-02-07 23:50:00 +0700

    edit department

--- src/components/DepartmentComponent.js
+++ src/components/DepartmentComponent.js
@@ -1,24 +1,30 @@
-import React from 'react';
-import {Card, CardText, CardBody, CardTitle} from 'reactstrap';
+import React from "react";
+import { Card } from "reactstrap";
+// Presentational Component
+const RenderDepartment = ({ department }) => {
+  return (
+    <Card className="p-3">
+      <h4 className="py-3">{department.name}</h4>
+      <p>Số lượng nhân viên: {department.numberOfStaff}</p>
+    </Card>
+  );
+};
 
-
+// Container Component
 function Department(props) {
+  const department = props.department.map((department) => {
     return (
-      <Card className="col-12 col-sm-6 col-md-4">
-        <CardTitle>Phòng ban</CardTitle>
-        <CardBody>
-          {props.department.map((department) => {
-            return (
-              <>
-                <CardTitle>{department.name}</CardTitle>
-                <CardText>Số lượng nhân viên: {department.numberOfStaff}</CardText>
-              </>
-            );
-          })} 
-        </CardBody>
-      </Card>
+      <div className="col-lg-4 col-md-6 col-12" key={department.id}>
+        <RenderDepartment department={department} />
+      </div>
     );
+  });
+
+  return (
+    <div className="container">
+      <div className="row">{department}</div>
+    </div>
+  );
 }
 
 export default Department;
-
