7935ea2fac0afac52994ad498f27d1c2ca31cd64
hang phuong <xna.giotnang@gmail.com>
2022-02-14 22:28:37 +0700

    : Presentational and Container Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,51 +1,33 @@
-import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardTitle,
-} from "reactstrap";
-import Dishdetail from "./DishdetailComponent";
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay,
+  CardTitle } from 'reactstrap';
+
 
 class Menu extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      selectedDish: null,
-      dishes: props.dishes
-    };
-  }
-
-  onDishSelect(dish) {
-    this.setState({ selectedDish: dish });
-  }
-
 
   render() {
-    
-    const menu = this.state.dishes.map((dish) => {
-      // console.log(this.state.selectedDish)
-      return (
-        <div key={dish.id} className="col-12 col-md-5 m-1">
-          <Card onClick={() => this.onDishSelect(dish)}>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
-            <CardImgOverlay>
-              <CardTitle>{dish.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
-        </div>
-      );
-    });
 
-    return (
-      <div className="container">
-        <div className="row">
-          {menu}
-        </div>                 
-        <Dishdetail dish={this.state.selectedDish} />      
-      </div>
-    );
+      const menu = this.props.dishes.map((dish) => {
+          return (
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+              <Card
+                onClick={() => this.props.onClick(dish.id)}>
+                <CardImg width="100%" src={dish.image} alt={dish.name} />
+                <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                </CardImgOverlay>
+              </Card>
+            </div>
+          );
+      });
+
+      return (
+          <div className="container">
+              <div className="row">
+                  {menu}
+              </div>
+          </div>
+      );
   }
 }
 
