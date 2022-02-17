a63a1c564e1f2f90b1c95c82bb4d41449d1e7f18
hang phuong <xna.giotnang@gmail.com>
2022-02-17 23:04:29 +0700

    Controlled Forms

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,12 +3,23 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 
 function About(props) {
-
-    const leaders = props.leaders.map((leader) => {
+    
+    const RenderLeader = props.leaders.map((leader) => {
         return (
-            <p>Leader {leader.name}</p>
-        );
-    });
+            <div key={leader.id} className="col-12 mt-5">
+                <Media tag="li">
+                  <Media left middle>
+                      <Media object src={leader.image} alt={leader.name} />
+                  </Media>
+                  <Media body className="ml-5">
+                    <Media heading>{leader.name}</Media>
+                    <p>{leader.designation}</p>
+                    <p>{leader.description}</p>
+                  </Media>
+                </Media>
+              </div>
+            );
+        });
 
     return(
         <div className="container">
@@ -66,7 +77,7 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {leaders}
+                        {RenderLeader}
                     </Media>
                 </div>
             </div>
