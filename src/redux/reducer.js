6b92a665551f6e69614a0d22e3e35ca934195e5c
19962608Na <95696785+19962608Na@users.noreply.github.com>
2022-02-24 10:31:42 +0700

    Update reducer.js
    chú thích cho reducer (1)

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -10,6 +10,12 @@ export const initialState = {
     leaders: LEADERS
 };
 
+//chức năng reducer sẽ nhận đc state hiện tại và sau đó là 1 hành động => để tạo state tiếp theo cần state hiện tại và 1 action
+//vì là function thuần tuý nên k thể sửa đổi state trực tiếp tại đây
+//ES6: chỉ định giá trị mặc định cho 1 tham số, khi store gọi reducer lần đầu tiên, store sẽ k có state vì vậy state sẽ k đc khởi tạo, do đó
+//state = initialState có nghĩa là khi k có state thì giá trị mặc định là initialState sẽ đc dùng
 export const Reducer = (state = initialState, action) => {
+    //chưa có hành đông nào nên mặc định trả lại state
     return state;
 };
+//cần có hàm reducer vì store cần biết pahir làm j khi 1 action đc thực hiện
