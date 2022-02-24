07e50f870d94391860da842cc4ca97da4f8701fd
19962608Na <95696785+19962608Na@users.noreply.github.com>
2022-02-24 10:35:50 +0700

    Update configureStore.js
    chú thích cho store (2)

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,7 @@
 import {createStore} from 'redux';
 import { Reducer, initialState } from './reducer'
 
+//hàm ConfigureStore là bắt buộc vì đó là cách định cấu hình store
 export const ConfigureStore = () => {
     const store = createStore(
         Reducer, // reducer
@@ -9,3 +10,4 @@ export const ConfigureStore = () => {
 
     return store;
 }
+//khi ai đó gọi ConfigureStore thì nó sẽ cấu hình Redux Store
