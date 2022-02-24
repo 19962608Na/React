d3ad2a9d05b9717bece104398c4de9805fc7c417
19962608Na <95696785+19962608Na@users.noreply.github.com>
2022-02-24 11:16:54 +0700

    Update App.js
    chú thích app.js(3)

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+//provider cho phép định cấu hình ứng dụng để redux store có sẵn cho tất cả các cpn trong ứng dụng
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
 const store = ConfigureStore();
 
-
-
 class App extends Component {
   render() {
     return (
+      //bọc provider bên ngoài browser route, provider lấy 1 thuộc tính là thuộc tính store
+      //đảm bảo rằng store có sẵn trong các cpn
       <Provider store={store}>
         <BrowserRouter>
           <div className="App">
